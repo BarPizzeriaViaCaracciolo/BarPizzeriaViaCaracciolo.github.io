7456d9f1ce4e7fab3a1c9de4cff3290214b0051a
BarPizzeriaViaCaracciolo <102189238+BarPizzeriaViaCaracciolo@users.noreply.github.com>
2022-04-24 14:34:07 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -190,14 +190,6 @@
     aos_init();
   });
   
-  function switchStyle() {
-  if (document.getElementById('styleSwitch').checked) {
-    document.getElementById('gallery').classList.add("custom");
-    document.getElementById('exampleModal').classList.add("custom");
-  } else {
-    document.getElementById('gallery').classList.remove("custom");
-    document.getElementById('exampleModal').classList.remove("custom");
-  }
-}
+  
 
 })(jQuery);
